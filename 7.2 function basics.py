faff720e9dad5b41aae4202afd152b94435b9c7d
sakinahsk <97861709+sakinahsk@users.noreply.github.com>
2022-02-08 18:20:03 +0800

    Add files via upload

---
+++ 7.2 function basics.py
@@ -0,0 +1,14 @@
+def hello_world():
+    print("Hello, World!")  
+hello_world()
+
+def print_name(name):
+    print(f"Name is {name}")    
+
+print_name("Keith")
+output = print_name("Keith")
+
+def add_two(num):
+    return num + 2
+result = add_two(2)
+print(result)
